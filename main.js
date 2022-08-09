411d5aa59878b9f1f6d3262b28d738a1479b63e9
d4niel <olayajuandaniel8@gmail.com>
2022-08-09 10:41:20 -0500

    Assets

---
+++ main.js
@@ -0,0 +1,6 @@
+const app = document.getElementById('app');
+const template = `<h1>Ecommerce</h1>
+<a href="/pages/card/index.html">Cart</a>
+<a href="/pages/products/index.html">Product</a>
+<a href="/pages/shop/index.html">Shop</a>`;
+app.innerHTML = template;
