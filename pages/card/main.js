93d9144e199fe1dc901deab4040dd56afdd97be7
d4niel <olayajuandaniel8@gmail.com>
2022-08-10 10:34:45 -0500

    Vista login

--- pages/card/main.js
+++ pages/card/main.js
@@ -1,8 +1,9 @@
 import * as Navbar from '../components/navbar/main.js';
 import * as Login from '../components/login/main.js';
-
+import * as Footer from '../components/footer/main.js';
 const header = document.getElementById('header');
 const main = document.getElementById('main');
 const footer = document.getElementById('footer');
 // Navbar.render(header);
 Login.render(main);
+Footer.render(footer);
