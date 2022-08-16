ef168fd1976ef599205be3a1019f9abb5c7bf75d
d4niel <olayajuandaniel8@gmail.com>
2022-08-15 19:21:28 -0500

    Estilos globales

--- pages/components/navbar/main.js
+++ pages/components/navbar/main.js
@@ -1,6 +1,8 @@
 import NavTemplate from './index.html?raw';
 
-
+const RenderBtn = (route) =>{
+    return ``
+}
 
 export function render(dom){
     dom.innerHTML = NavTemplate;
--- pages/shop/main.js
+++ pages/shop/main.js
@@ -1,9 +1,12 @@
 import { getPopular } from "../service/api_service";
 import * as Movie from '../components/movie/index';
-
+import * as Nav from '../components/navbar/main';
 if(localStorage.getItem('sesion') == 'false'){
     window.location.pathname = '/pages/card/index.html';
 }
 const container = document.getElementById('main');
 const listado = await getPopular();
+
+const header = document.getElementById('header');
+Nav.render(header);
 Movie.render(container,listado);
