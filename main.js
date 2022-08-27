b18056b18ef1246771090a0dd98668128f900370
d4niel <olayajuandaniel8@gmail.com>
2022-08-27 09:51:21 -0500

    Reset

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-import * as TemplateShop from './pages/shop/index.js';
-
-
-TemplateShop.render();
+const app = document.getElementById('app');
+const template = `<h1>Ecommerce</h1>
+<a href="/pages/card/index.html">Cart</a>
+<a href="/pages/products/index.html">Product</a>
+<a href="/pages/shop/index.html">Shop</a>`;
+app.innerHTML = template;
