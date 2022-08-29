ee38fb0f1f4a4d94de2579d0bf5056b8699dc0da
d4niel <olayajuandaniel8@gmail.com>
2022-08-29 09:31:07 -0500

    Render class

--- components/movies/main.js
+++ components/movies/main.js
@@ -1,20 +1,29 @@
-const CreateCard = ({id,title, backdrop_path}) =>{
-    const url = 'https://image.tmdb.org/t/p/w500';
-    const card = document.createElement('article');
-    const parrafo = document.createElement('p');   
-    const img = document.createElement('img');
-    parrafo.textContent = title;
-    img.src = url+backdrop_path;
-    card.appendChild(parrafo);
-    card.appendChild(img);
-    return card;
+export class Movies
+{
+    constructor(dom, list){
+        this.url =  'https://image.tmdb.org/t/p/w500';
+        this.dom = dom;
+        this.list = list;
+    }
 
-}
-
-export function render(dom,listado){
-    const frag = document.createDocumentFragment();
-    listado.forEach(el=>{
-        frag.appendChild(CreateCard(el));
-    })
-    dom.appendChild(frag);
+    Create({id,title, backdrop_path}){
+        const card = document.createElement('article');
+        const text = document.createElement('p');
+        const img = document.createElement('img');
+        const button = document.createElement('button');
+        text.textContent = title;
+        img.src = this.url+backdrop_path;
+        button.textContent = 'Add to cart';
+        card.appendChild(text);
+        card.appendChild(img);
+        card.appendChild(button);
+        return card;
+    }
+    render(){
+        const frag = document.createDocumentFragment('frag');
+        this.list.forEach(element => {
+            frag.appendChild(this.Create(element));
+        });
+        this.dom.appendChild(frag);
+    }
 }
---
+++ pages/shop/main.js
@@ -0,0 +1,7 @@
+import { Movies } from "../../components/movies/main";
+import { get } from "../../service/api";
+const app = document.getElementById('app');
+const list = await get('movie/popular');
+const mov = new Movies(app,list.results);
+console.log(list)
+mov.render();
