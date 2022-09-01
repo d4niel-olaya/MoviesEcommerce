d5948546f4ff0db982233f26d1be44a6173e3143
d4niel <olayajuandaniel8@gmail.com>
2022-09-01 18:10:20 -0500

    Navbar with icons

--- components/navbar/main.js
+++ components/navbar/main.js
@@ -7,7 +7,15 @@ export class NavBar
     }
     Create(){
         const container = document.createElement('section');
-        container.innerHTML = `<img src="../../assets/icons/logo_yard_sale.svg">`
+        container.setAttribute('class', 'nav-items');
+        container.innerHTML = `
+        <img src="../../assets/icons/icon_menu.svg">
+        <img src="../../assets/icons/logo_yard_sale.svg">
+        <img src="../../assets/icons/icon_shopping_cart.svg">
+        <div>
+        <input type="search" placeholder="Buscar" class="search">
+        </div>`
+        
         return container;
     }
     render(){
