5d2acc172484d4ea7c499f749ff71e7fcdadfc6c
d4niel <olayajuandaniel8@gmail.com>
2022-09-01 19:37:19 -0500

    Search and cart

--- components/navbar/main.js
+++ components/navbar/main.js
@@ -11,16 +11,30 @@ export class NavBar
         container.innerHTML = `
         <img src="../../assets/icons/icon_menu.svg">
         <img src="../../assets/icons/logo_yard_sale.svg">
-        <img src="../../assets/icons/icon_shopping_cart.svg">
-        <div>
-        <input type="search" placeholder="Buscar" class="search">
-        </div>`
-        
+        <img src="../../assets/icons/icon_shopping_cart.svg">`
+        const cart = container.children[2];
+
+        this.OpenCart(cart);
+        return container;
+    }
+    OpenCart(item){
+        item.addEventListener('click', () =>{
+            const movies = JSON.parse(localStorage.getItem('movies'));
+            console.log(movies);
+        })
+    }
+    CreateSearch(){
+        const container = document.createElement('div');
+        container.setAttribute('class', 'search'); 
+        container.innerHTML = `
+        <input type="search" class="inp-search" placeholder="Buscar peli">
+        `
         return container;
     }
     render(){
         const frag = document.createDocumentFragment();
         frag.append(this.Create());
+        frag.append(this.CreateSearch());
         this.dom.appendChild(frag);
     }
 }
