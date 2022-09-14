534be7c9e30b100b5cca977b7c8746052ebae2a7
d4niel <olayajuandaniel8@gmail.com>
2022-09-14 10:35:58 -0500

    Recommended movies

--- components/product/main.js
+++ components/product/main.js
@@ -3,16 +3,18 @@ export class Product
 {
     constructor(obj, dom){
         this.obj = obj;
+        this.url =  'https://image.tmdb.org/t/p/w500';
         this.dom = dom;
         console.log(this.obj);
         this.render()
     }
 
-    CreateMovie({id,title}){
+    CreateMovie({id,title,backdrop_path}){
         const container = document.createElement('article');
         container.setAttribute('class', 'movie');
         const items = `
-        <p>${id}</p>
+        <a href="../../pages/product/index.html?id=${id}">Ver Detalles</a>
+        <img src="${this.url+backdrop_path}" alt="${title}">
         <p>${title}</p>
         `;
         container.insertAdjacentHTML('afterbegin', items);
