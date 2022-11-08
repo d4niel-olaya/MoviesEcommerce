d6f06622a2949faaf48a5a3348218815aa78a0ee
d4niel <olayajuandaniel8@gmail.com>
2022-11-08 09:37:56 -0500

    Splide carrousel

--- pages/product/main.js
+++ pages/product/main.js
@@ -2,6 +2,12 @@ import { Carrousel } from "../../components/carrousel/carrousel";
 import { get } from '../../service/api';
 import { Product } from "../../components/product/main";
 import { CarrouselV2 } from "../../components/carrousel/carrouselv2";
+// Default theme
+import '@splidejs/splide/css';
+
+import Splide from '@splidejs/splide';
+
+
 const url = new URL(window.location);
 const params = new URLSearchParams(url.search);
 const id = params.get('id');
@@ -10,5 +16,4 @@ const mv = await get(`movie/${id}`);
 const movie = await get(`movie/${id}/recommendations`);
 const casting = await get(`movie/${id}/credits`);
 const producto = new Product(movie.results, app, casting.cast, mv)
-const carrousel = new CarrouselV2(movie.results, app);
-
+new Splide( '.splide' ).mount();
